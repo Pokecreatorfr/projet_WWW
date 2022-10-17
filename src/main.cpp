91ed992b76e6a655154636f8d79a18bd75d262bc
Mathéo Vignaud <64284499+Pokecreatorfr@users.noreply.github.com>
2022-10-17 16:12:06 +0200

    add structs

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,45 @@
 #include <Arduino.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <config.h>
 
-#define ECHANTILLON_MOY 10;
-#define NB_CAPTEURS 10;
+#define ECHANTILLON_MOY 10
+#define NB_CAPTEURS 10
 
-int LOG_INTERVALL=10;
-int FILE_MAX_SIZE=4096;
-int TIMEOUT=30;
+int mode = 0;/*0 standard ; 1 eco ; 2 maintenance || définit le mode actuel  */
+#define STANDARD 0 
+#define ECO 1
+#define MAINTENANCE 2
 
-typedef struct capteur
+int LOG_INTERVALL=10; // intervale entre 2 mesures
+int FILE_MAX_SIZE=4096; // taille d'un fichier log
+int TIMEOUT=30; //
+
+typedef struct element
+{
+  int data;
+  struct element *suivant;
+}element;
+
+typedef struct
 {
   int value;
   bool error;
   float moyenne;
-  int tab[ECHANTILLON_MOY];
-};
+  element *premier_element;
+}capteur;
 
 
-void setup() {
-  int *tabptr[]
+void setup() 
+{
+  int *tabptr[NB_CAPTEURS];
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+void changement_mode(int newmode)
+{
+  mode = newmode;
 }
