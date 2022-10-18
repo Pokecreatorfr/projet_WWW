299ddd72b9fead427c248b96707117f406e989d2
Mathéo Vignaud <64284499+Pokecreatorfr@users.noreply.github.com>
2022-10-18 22:24:13 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,13 @@
 #define ECHANTILLON_MOY 10
 #define NB_CAPTEURS 10
 
+#define DEBUG
+
+#ifdef DEBUG
+#define CONFIG_MODE_TIME 1
+#else
+#define CONFIG_MODE_TIME 30
+#endif
 
 ChainableLED led(6, 7, 1);
 
@@ -16,10 +23,12 @@ int red[3]    = {255,0,0};
 int orange[3] = {255,128,0};
 int yellow[3] = {255,255,0};
 int green[3]  = {0,255,0};
+int blue[3]   = {0,0,255};
 int white[3]  = {255,255,255};
 
 
 int mode = 1;/*0 maintenance ; 1 standard  ; 2 eco || définit le mode actuel  */
+int previous_mode = 1;
 #define MAINTENANCE 0 
 #define STANDARD    1
 #define ECO         2
@@ -27,8 +36,8 @@ int mode = 1;/*0 maintenance ; 1 standard  ; 2 eco || définit le mode actuel  *
 //variables pour chauqe boutons servant dans le changement de mode
 bool stepredbutton = false;
 bool stepgreenbutton = false;
-unsigned long red_timer , green_timer;
-const byte redbutton = 4;
+unsigned long red_timer , green_timer , config_timer;
+const byte redbutton = 2;
 const byte greenbutton = 3;
 
 #define No_GPS  5
@@ -75,6 +84,7 @@ void mesure_capteurs(capteur *pointeur);
 void changemode_red_button();
 void changemode_green_button();
 void set_led_color(int rgb[3]);
+void changement_mode(int newmode);
 
 void setup() 
 {
@@ -84,11 +94,39 @@ void setup()
   pinMode(greenbutton,INPUT);
   attachInterrupt(digitalPinToInterrupt(redbutton),changemode_red_button ,CHANGE);
   attachInterrupt(digitalPinToInterrupt(greenbutton),changemode_green_button ,CHANGE);
+
+  //noInterrupts();
+  if ( digitalRead(redbutton) == HIGH)
+  {
+    changement_mode(4);
+    set_led_color(yellow);
+    config_timer = millis();
+    while (millis() - config_timer < CONFIG_MODE_TIME * 60000)
+    {
+      Serial.println("config mode");
+    }
+    changement_mode(1);
+  }
+  //interrupts();
+
+
   set_led_color(red);
 }
 
 void loop() 
 {
+  switch (mode)
+  {
+  case 0:
+    set_led_color(orange);
+    break;
+  case 1:
+    set_led_color(green);
+    break;
+  case 2:
+    set_led_color(blue);
+    break;
+  }
   capteur *tabptr[NB_CAPTEURS];
   int i ;
   if (mode > 0)
@@ -130,7 +168,11 @@ void loop()
 
 void changement_mode(int newmode)
 {
+  previous_mode = mode;
   mode = newmode;
+  #ifdef DEBUG
+  Serial.println(mode);
+  #endif
 }
 
 void changemode_red_button()
@@ -138,31 +180,37 @@ void changemode_red_button()
   double long a;
   double long b;
   
-  if (stepredbutton == false) 
+
+
+  if (stepredbutton == false  && mode != 4) 
   {
-  stepredbutton = true;
-  red_timer = millis();
+    stepredbutton = true;
+    red_timer = millis();
   }
   else if( millis() - red_timer >= 5000)
   {
     switch (mode)
     {
     case 0:
-      
+      changement_mode(previous_mode);
       break;
     
     case 1:
-
+      changement_mode(0);
       break;
     
     case 2:
-
+      changement_mode(0);
       break;
     }
-    stepredbutton = true;
+    stepredbutton = false;
   }
-  else stepredbutton = true;
-  for (a=0; a<10000 ; a++) //cela prend environ 100ms a s'executer et remplace donc delay(100)
+  else stepredbutton = false;
+  #ifdef DEBUG
+  Serial.print("red button time :" );
+  Serial.println( millis() - red_timer);
+  #endif
+  for (a=0; a<50000 ; a++) //cela prend assez de temps a s'executer pour remplacer delay() Nombre d'iteration encore a definir
   {
     b++;
   }
@@ -173,26 +221,35 @@ void changemode_green_button()
   double long a;
   double long b;
   
-  if (stepgreenbutton == false) stepgreenbutton = true;
-  else
+  if (stepgreenbutton == false && mode != 4 ) 
+  {
+  stepgreenbutton = true;
+  green_timer = millis();
+  }
+  else if (millis() - green_timer >= 5000)
   {
     switch (mode)
     {
     case 0:
-      
+      ;
       break;
     
     case 1:
-
+      changement_mode(2);
       break;
     
     case 2:
-
+      changement_mode(1);
       break;
     }
-    stepgreenbutton = true;
+    stepgreenbutton = false;
   }
-  for (a=0; a<10000 ; a++) //cela prend environ 100ms a s'executer et remplace donc delay(100)
+  else stepgreenbutton = false;
+  #ifdef DEBUG
+  Serial.print("green button time:" );
+  Serial.println( millis() - green_timer);
+  #endif
+  for (a=0; a<50000 ; a++) //cela prend assez de temps a s'executer pour remplacer delay() Nombre d'iteration encore a definir
   {
     b++;
   }
